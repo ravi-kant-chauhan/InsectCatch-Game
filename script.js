9deefa40de55ebef20df29ca880e1de14b591401
Ravi <ravkchauhan1aip@gmail.com>
2023-01-30 23:55:49 +0530

    final

--- script.js
+++ script.js
@@ -58,3 +58,21 @@ function getRandomLocation() {
     return {x, y}
 }
 
+function catchInsect() {
+    increaseScore()
+    this.classList.add('caught')
+    setTimeout(() => this.remove(), 2000)
+    addInsects()
+}
+function addInsects() {
+    setTimeout(createInsect, 1000)
+    setTimeout(createInsect, 1500)
+}
+
+function increaseScore() {
+    score++
+    if(score > 19) {
+        message.classList.add('visible')
+    }
+    scoreEl.innerHTML = `Score: ${score}`
+}
